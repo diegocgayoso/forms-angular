6a8d786d733cd88550ea99efe52bf81b06addb25
diegocgayoso <diegoc.gayoso@gmail.com>
2022-11-21 16:38:32 -0300

    Correção branch aula2

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,6 +1,5 @@
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
